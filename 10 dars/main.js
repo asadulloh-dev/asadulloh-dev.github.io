0499a55c9060df52ba67fa063965145dea23ae49
Asadbek <asadbek.3422@gmail.com>
2023-04-30 07:07:36 +0500

    First commit

---
+++ 10 dars/main.js
@@ -0,0 +1,173 @@
+let answers1 = document.querySelectorAll(".a1 p");
+let result1 = document.querySelector(".r1");
+let ball = 0;
+answers1[0].addEventListener("click", function () {
+  a1.innerHTML = "to'gri";
+  a1.classList.add("true");
+  ball += 1;
+  score.innerHTML = ball;
+});
+answers1[1].addEventListener("click", function () {
+  a1.innerHTML = "Noto'gri";
+  a1.classList.add("false");
+});
+answers1[2].addEventListener("click", function () {
+  a1.innerHTML = "Noto'gri";
+  a1.classList.add("false");
+});
+
+let answers2 = document.querySelectorAll(".a2 p");
+let result2 = document.querySelector(".r2");
+answers2[0].addEventListener("click", function () {
+  a2.innerHTML = "to'gri";
+  a2.classList.add("true");
+  ball += 1;
+  score.innerHTML = ball;
+  
+});
+answers2[1].addEventListener("click", function () {
+  a2.innerHTML = "Noto'gri";
+  a2.classList.add("false");
+});
+answers2[2].addEventListener("click", function () {
+  a2.innerHTML = "Noto'gri";
+  a2.classList.add("false");
+});
+
+let answers3 = document.querySelectorAll(".a3 p");
+let result3 = document.querySelector(".r3");
+answers3[0].addEventListener("click", function () {
+  a3.innerHTML = "to'gri";
+  a3.classList.add("true");
+  ball += 1;
+  score.innerHTML = ball;
+});
+answers3[1].addEventListener("click", function () {
+  a3.innerHTML = "Noto'gri";
+  a3.classList.add("false");
+});
+answers3[2].addEventListener("click", function () {
+  a3.innerHTML = "Noto'gri";
+  a3.classList.add("false");
+
+});
+
+let answers4 = document.querySelectorAll(".a4 p");
+let result4 = document.querySelector(".r4");
+answers4[0].addEventListener("click", function () {
+  a4.innerHTML = "Noto'gri";
+  a4.classList.add("false");
+});
+answers4[1].addEventListener("click", function () {
+  a4.innerHTML = "to'gri";
+  a4.classList.add("true");
+  ball += 1;
+  score.innerHTML = ball;
+});
+answers4[2].addEventListener("click", function () {
+  a4.innerHTML = "Noto'gri";
+  a4.classList.add("false");
+});
+
+let answers5 = document.querySelectorAll(".a5 p");
+let result5 = document.querySelector(".r5");
+answers5[0].addEventListener("click", function () {
+  a5.innerHTML = "Noto'gri";
+  a5.classList.add("false");
+});
+answers5[1].addEventListener("click", function () {
+  a5.innerHTML = "Noto'gri";
+  a5.classList.add("false");
+});
+answers5[2].addEventListener("click", function () {
+  a5.innerHTML = "to'gri";
+  a5.classList.add("true");
+  ball += 1;
+  score.innerHTML = ball;
+});
+
+let answers6 = document.querySelectorAll(".a6 p");
+let result6 = document.querySelector(".r6");
+answers6[0].addEventListener("click", function () {
+  a6.innerHTML = "to'gri";
+  a6.classList.add("true");
+  ball += 1;
+  score.innerHTML = ball;
+});
+answers6[1].addEventListener("click", function () {
+  a6.innerHTML = "Noto'gri";
+  a6.classList.add("false");
+});
+answers6[2].addEventListener("click", function () {
+  a6.innerHTML = "Noto'gri";
+  a6.classList.add("false");
+});
+
+let answers7 = document.querySelectorAll(".a7 p");
+let result7 = document.querySelector(".r7");
+answers7[0].addEventListener("click", function () {
+  a7.innerHTML = "to'gri";
+  a7.classList.add("true");
+  ball += 1;
+  score.innerHTML = ball;
+});
+answers7[1].addEventListener("click", function () {
+  a7.innerHTML = "Noto'gri";
+  a7.classList.add("false");
+});
+answers7[2].addEventListener("click", function () {
+  a7.innerHTML = "Noto'gri";
+  a7.classList.add("false");
+});
+
+let answers8 = document.querySelectorAll(".a8 p");
+let result8 = document.querySelector(".r8");
+answers8[0].addEventListener("click", function () {
+  a8.innerHTML = "Noto'gri";
+  a8.classList.add("false");
+});
+answers8[1].addEventListener("click", function () {
+  a8.innerHTML = "Noto'gri";
+  a8.classList.add("false");
+});
+answers8[2].addEventListener("click", function () {
+  a8.innerHTML = "to'gri";
+  a8.classList.add("true");
+  ball += 1;
+  score.innerHTML = ball;
+});
+
+let answers9 = document.querySelectorAll(".a9 p");
+let result9 = document.querySelector(".r9");
+answers9[0].addEventListener("click", function () {
+  a9.innerHTML = "Noto'gri";
+  a9.classList.add("false");
+});
+answers9[1].addEventListener("click", function () {
+  a9.innerHTML = "Noto'gri";
+  a9.classList.add("false");
+});
+answers9[2].addEventListener("click", function () {
+  
+  a9.innerHTML = "to'gri";
+  a9.classList.add("true");
+  ball += 1;
+  score.innerHTML = ball;
+});
+
+let answers10 = document.querySelectorAll(".a10 p");
+let result10 = document.querySelector(".r10");
+answers10[0].addEventListener("click", function () {
+  a10.innerHTML = "Noto'gri";
+  a10.classList.add("false");
+});
+answers10[1].addEventListener("click", function () {
+  a10.innerHTML = "to'gri";
+  a10.classList.add("true");
+  ball += 1;
+  score.innerHTML = ball;
+});
+answers10[2].addEventListener("click", function () {
+  a10.innerHTML = "Noto'gri";
+  a10.classList.add("false");
+});
